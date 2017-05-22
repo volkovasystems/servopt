b71ebb3b895b665af86d45fbe169241cbb6b881b
johnlenon <johnlenonmaghanoy@gmail.com>
2017-05-22 13:55:58 +0800

    Updated test.

--- servopt-test.js
+++ servopt-test.js
@@ -1,7 +1,6 @@
 const assert = require( "assert" );
 const servopt = require( "./servopt.js" );
 
-assert.ok( servopt( { "production": { "server": { "protocol": "https" } } } ),
-{ "showHidden": true, "depth": Infinity } );
+assert.ok( servopt( { "production": { "server": { "protocol": "https" } } } ), { "showHidden": true, "depth": Infinity } );
 
 console.log( "ok" );
